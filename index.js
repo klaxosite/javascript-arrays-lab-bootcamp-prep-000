3e15d2d05bc6adbc0db8346688191611ace28cc6
Jastien Sandhu <superjastin@gmail.com>
2020-05-14 01:55:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  
+}
+
+function destructivelyPrependKitten(name){
+  
+}
+
+function destructivelyRemoveLastKitten(name){
+  
+}
+
+function destructivelyRemoveFirstKitten(name){
+  
+}
